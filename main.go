cc06785bd4f79557e4bd1e8eb5774921a1ebabc7
TheLuisADL <luis.bolivar@avaldigitallabs.com>
2021-01-15 20:25:55 -0500

    Add class about variables, and different types of declarations

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola mundo!");
+var x, y, w = true, 3, "aY"
+
+//go run main.go
+func main() {
+	var a int
+	n := 3.212
+	o, p, q := false, "help!", 43534534
+	fmt.Println(a, x, y, w, n, o, p, q)
 }
