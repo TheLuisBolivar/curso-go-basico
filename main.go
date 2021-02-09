5504e2b71cbd55ad6a5fbed584c2c92197efad01
TheLuisADL <luis.bolivar@avaldigitallabs.com>
2021-02-09 13:31:40 -0500

    Add class about variables, and different types of declarations

--- main.go
+++ main.go
@@ -1,13 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
 
-var x, y, w = true, 3, "aY"
+const (
+	ConstX = 43
+	ConstY = 45.5
+	ConstU = "hello world"
+)
+
+var (
+	ToBe       bool       = false
+	MaxInt     uint64     = 1<<61 - 1
+	z          complex128 = cmplx.Sqrt(-5 + 12i)
+	w          int
+	y          bool
+	s          string
+	pruebaCast float64 = 64.43
+)
 
 //go run main.go
 func main() {
-	var a int
-	n := 3.212
-	o, p, q := false, "help!", 43534534
-	fmt.Println(a, x, y, w, n, o, p, q)
+	i := int(pruebaCast)
+	fmt.Printf("Type: %T value: %v \n", ToBe, ToBe)
+	fmt.Printf("Type: %T value: %v \n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T value: %v \n", z, z)
+	fmt.Printf("Type: %T value: %v \n", w, w)
+	fmt.Printf("Type: %T value: %v \n", y, y)
+	fmt.Printf("Type: %T value: %v \n", s, s)
+	fmt.Printf("Type: %T value: %v \n", pruebaCast, pruebaCast)
+	fmt.Printf("Type: %T value: %v \n", i, i)
+	fmt.Printf("Type: %T value: %v \n", ConstX, ConstX)
+	fmt.Printf("Type: %T value: %v \n", ConstY, ConstY)
+	fmt.Printf("Type: %T value: %v \n", ConstU, ConstU)
 }
