ac36b4b2bbdce452c94a44669eac609a102c7dfa
TheLuisADL <luis.bolivar@avaldigitallabs.com>
2021-02-24 09:38:47 -0500

    finish flow control review

--- main.go
+++ main.go
@@ -1,38 +1,120 @@
 package main
 
-import (
-	"fmt"
-	"math/cmplx"
-)
-
-const (
-	ConstX = 43
-	ConstY = 45.5
-	ConstU = "hello world"
-)
-
-var (
-	ToBe       bool       = false
-	MaxInt     uint64     = 1<<61 - 1
-	z          complex128 = cmplx.Sqrt(-5 + 12i)
-	w          int
-	y          bool
-	s          string
-	pruebaCast float64 = 64.43
-)
+import "fmt"
 
 //go run main.go
 func main() {
-	i := int(pruebaCast)
-	fmt.Printf("Type: %T value: %v \n", ToBe, ToBe)
-	fmt.Printf("Type: %T value: %v \n", MaxInt, MaxInt)
-	fmt.Printf("Type: %T value: %v \n", z, z)
-	fmt.Printf("Type: %T value: %v \n", w, w)
-	fmt.Printf("Type: %T value: %v \n", y, y)
-	fmt.Printf("Type: %T value: %v \n", s, s)
-	fmt.Printf("Type: %T value: %v \n", pruebaCast, pruebaCast)
-	fmt.Printf("Type: %T value: %v \n", i, i)
-	fmt.Printf("Type: %T value: %v \n", ConstX, ConstX)
-	fmt.Printf("Type: %T value: %v \n", ConstY, ConstY)
-	fmt.Printf("Type: %T value: %v \n", ConstU, ConstU)
+	var frontier int = 15
+	result := whileFunction(frontier)
+	ifValidation := ifElseTest(frontier)
+	switchValidation := switchFunction(frontier)
+	switchWithoutValue := switchWithoutExpression(frontier)
+	frontierDefer := deferTest(frontier)
+	frontierDefer2 := deferTest2(frontier)
+	fmt.Printf("value type is: %T, and value is: %v \n", result, result)
+	fmt.Printf("value type is: %T, and value is: %v \n", ifValidation, ifValidation)
+	fmt.Printf("value type is: %T, and value is: %v \n", switchValidation, switchValidation)
+	fmt.Printf("value type is: %T, and value is: %v \n", switchWithoutValue, switchWithoutValue)
+	fmt.Printf("value type is: %T, and value is: %v \n", frontierDefer, frontierDefer)
+	fmt.Printf("value type is: %T, and value is: %v \n", frontierDefer2, frontierDefer2)
+}
+
+func deferTest(frontier int) int {
+	defer switchFunction(frontier)
+	frontier++
+	fmt.Printf("[deferTest]: value frontier its: %v", frontier)
+	return frontier
+}
+
+func deferTest2(frontier int) int {
+	fmt.Print("[deferTest2] Starting")
+
+	for i := 0; i < frontier; i++ {
+		defer fmt.Printf("[deferTest2] value : %v \n", i)
+	}
+
+	fmt.Print("[finish]")
+	frontier++
+	return frontier
+}
+
+func switchWithoutExpression(frontier int) bool {
+	result := false
+
+	switch {
+	case frontier == 15:
+		fmt.Println("frontier its 15")
+		result = true
+	case frontier > 15:
+		fmt.Println("frontier its 15")
+		result = false
+	}
+
+	return result
+}
+
+func switchFunction(frontier int) int {
+	fmt.Printf("[switchFunction]: frontier es %v", frontier)
+	result := 0
+	switch frontier {
+	case 15:
+		fmt.Println("El frontier es 15")
+		result = frontier + 1
+	case 16:
+		fmt.Println("El frontier es 16")
+		result = frontier - 1
+	default:
+		fmt.Println("El frontier es una cosa rara")
+	}
+
+	return result
+}
+
+func forFunction1(frontier int) int {
+	result := 0
+	for i := 0; i < frontier; i++ {
+		result += i
+	}
+
+	return result
+}
+
+func whileFunction(frontier int) int {
+	result := 1
+	for result < frontier {
+		result += result
+	}
+
+	return result
+}
+
+func ifTest(frontier int) bool {
+	result := false
+
+	if frontier != 16 {
+		result = true
+	}
+
+	return result
+}
+
+func ifTest2(frontier int) bool {
+	result := false
+	if frontier++; frontier != 16 {
+		result = true
+	}
+
+	return result
+}
+
+func ifElseTest(frontier int) bool {
+	result := false
+	if frontier++; frontier != 16 {
+		result = true
+	} else {
+		fmt.Println("Estamos en else bitches")
+		result = false
+	}
+
+	return result
 }
